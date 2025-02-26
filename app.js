d357a0ee77643e86d3c167d2565d045c0e4913d1
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-02-27 00:37:51 +0200

    Connect to MongoDB

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const path = require("path");
 const PORT = 3000;
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // Middleware
-app.use(express.json())
-app.use("/api/v1/tasks", tasks)
+app.use(express.json());
+app.use("/api/v1/tasks", tasks);
 app.use(express.static(path.join(__dirname, "public")));
-
 
 // Routes
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
 
-app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
