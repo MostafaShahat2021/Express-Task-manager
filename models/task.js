24a8490694314a6d8423898897408aa98776987c
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-03 21:29:39 +0200

    update name validatons message in task schema

--- models/task.js
+++ models/task.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const TaskSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please Provide Taske name"],
+    required: [true, "Please Provide Task name"],
     trim: true,
     maxlength: [20, "Task name should be less than 20 characters"],
     minlength: [3, "Task name Shoud be more than 3 characters"]
